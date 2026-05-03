6cb433c99134bd98680fa227e8dbf5c1f9afa05a
SuperficialCake <contact@superficialcake.dev>
2026-05-03 02:11:19 -0500

    Add Sunrise Tracking

--- src/client/kotlin/dev/superficialcake/dankhelper/handlers/ScoreboardHandler.kt
+++ src/client/kotlin/dev/superficialcake/dankhelper/handlers/ScoreboardHandler.kt
@@ -3,12 +3,14 @@ package dev.superficialcake.dankhelper.handlers
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.minecraft.ChatFormatting
 import net.minecraft.client.Minecraft
+import net.minecraft.world.scores.DisplaySlot
 import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
 
-object ScoreboardHandler{
-
+object ScoreboardHandler {
     private val logger = LoggerFactory.getLogger("DankHelper-Scoreboard")
     private var tickCounter = 0
+    private var prevTitleColor: Int? = null
     var formattedSessionBM: String = "0"
 
     private const val SCAN_INTERVAL = 10
@@ -16,20 +18,48 @@ object ScoreboardHandler{
     private var initialBM: Long = -1L
     private var lastSeenBM: Long = -1L
     var sessionBM: Long = 0L
+    var sunriseTime: String = "--:--"
 
-    fun init(){
-        ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client ->
-            tickCounter++
+    fun init() {
+        ClientTickEvents.END_CLIENT_TICK.register(
+            ClientTickEvents.EndTick { client ->
+                tickCounter++
 
-            if(tickCounter >= SCAN_INTERVAL){
-                tickCounter = 0
-                scanScoreboard(client)
-            }
-        })
+                if (tickCounter >= SCAN_INTERVAL) {
+                    tickCounter = 0
+                    scanScoreboard(client)
+                }
+            },
+        )
     }
 
     private fun scanScoreboard(client: Minecraft) {
         val scoreboard = client.level?.scoreboard ?: return
+        val sidebarObj = scoreboard.getDisplayObjective(DisplaySlot.SIDEBAR)
+        val titleText = sidebarObj?.displayName ?: return
+
+        var currentTitleColor = titleText.style.color?.value
+        if (currentTitleColor == null) {
+            currentTitleColor =
+                titleText.siblings
+                    .firstOrNull {
+                        it.string.isNotBlank() && it.style.color != null
+                    }?.style
+                    ?.color
+                    ?.value
+        }
+
+        if (prevTitleColor == null) {
+            prevTitleColor = currentTitleColor
+        } else if (prevTitleColor != currentTitleColor) {
+            val now = LocalDateTime.now()
+            val formattedTime = "0:%02d:%02d".format(now.minute % 10, now.second)
+
+            logger.info(formattedTime)
+            prevTitleColor = currentTitleColor
+
+            sunriseTime = formattedTime
+        }
 
         for (team in scoreboard.playerTeams) {
             val fullLine = ChatFormatting.stripFormatting(team.playerPrefix.string + team.playerSuffix.string) ?: ""
@@ -44,8 +74,8 @@ object ScoreboardHandler{
                     logger.info("Session Start BM captured: $initialBM")
                 }
 
-                if (currentTotalBM < lastSeenBM){
-                    initialBM -= lastSeenBM
+                if (currentTotalBM < lastSeenBM) {
+                    initialBM = lastSeenBM
                 }
 
                 lastSeenBM = currentTotalBM
@@ -56,9 +86,10 @@ object ScoreboardHandler{
         }
     }
 
-    fun reset(){
+    fun reset() {
         initialBM = -1L
         lastSeenBM = -1L
         sessionBM = 0L
+        prevTitleColor = null
     }
 }
