f81dd7ff901f17670a820d562d34f866b6094d7a
SuperficialCake <contact@superficialcake.dev>
2026-05-03 02:10:55 -0500

    Add Sunrise to config

--- src/client/kotlin/dev/superficialcake/dankhelper/config/DankConfig.kt
+++ src/client/kotlin/dev/superficialcake/dankhelper/config/DankConfig.kt
@@ -11,6 +11,7 @@ class DankConfig : ConfigData {
 
     // Toggles
     var showSessionTime: Boolean = true
+    var showSunriseTime: Boolean = true
     var showMPM: Boolean = true
     var showASMPM: Boolean = false
     var showTPM: Boolean = true
