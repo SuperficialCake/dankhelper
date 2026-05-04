3d873f9260fef54d9498113ef78a4f5651295770
SuperficialCake <contact@superficialcake.dev>
2026-05-04 15:01:53 -0500

    Removed unneeded logger

--- src/main/kotlin/dev/superficialcake/dankhelper/DankHelper.kt
+++ src/main/kotlin/dev/superficialcake/dankhelper/DankHelper.kt
@@ -7,9 +7,5 @@ object DankHelper : ModInitializer {
     private val logger = LoggerFactory.getLogger("dankhelper")
 
     override fun onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-        logger.info("Hello Fabric world!")
     }
 }
