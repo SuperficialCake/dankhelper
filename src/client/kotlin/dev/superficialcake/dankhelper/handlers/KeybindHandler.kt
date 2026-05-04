7fc593dd4d2b9d2c78b94fcf6f9d03375e32d654
SuperficialCake <contact@superficialcake.dev>
2026-05-04 13:03:11 -0500

    renamed translation keys to have consistent casing
    updated styling to follow KTLint standards

--- src/client/kotlin/dev/superficialcake/dankhelper/handlers/KeybindHandler.kt
+++ src/client/kotlin/dev/superficialcake/dankhelper/handlers/KeybindHandler.kt
@@ -3,23 +3,19 @@ package dev.superficialcake.dankhelper.handlers
 import com.mojang.blaze3d.platform.InputConstants
 import dev.superficialcake.dankhelper.DankHelper
 import dev.superficialcake.dankhelper.compat.buildConfigScreen
-import dev.superficialcake.dankhelper.config.DankConfig
 import dev.superficialcake.dankhelper.ui.EditHud
 import dev.superficialcake.dankhelper.util.UtilFunctions
-import me.shedaniel.autoconfig.AutoConfig
-import me.shedaniel.clothconfig2.api.ConfigBuilder
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper.registerKeyMapping
 import net.minecraft.client.KeyMapping
-import net.minecraft.network.chat.Component
 import net.minecraft.resources.Identifier
 import org.lwjgl.glfw.GLFW
 
 object KeybindHandler {
-
-    private val CATEGORY: KeyMapping.Category = KeyMapping.Category.register(
-        Identifier.fromNamespaceAndPath(DankHelper.MOD_ID, "general")
-    )
+    private val CATEGORY: KeyMapping.Category =
+        KeyMapping.Category.register(
+            Identifier.fromNamespaceAndPath(DankHelper.MOD_ID, "general"),
+        )
 
     lateinit var hideUIKey: KeyMapping
     lateinit var resetSessionKey: KeyMapping
@@ -29,58 +25,62 @@ object KeybindHandler {
     var showUI: Boolean = true
 
     fun init() {
-
-        resetSessionKey = registerKeyMapping(
-            KeyMapping(
-                "key.dankhelper.resetSessionStats",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_END,
-                CATEGORY
+        resetSessionKey =
+            registerKeyMapping(
+                KeyMapping(
+                    "key.dankhelper.resetSessionStats",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_END,
+                    CATEGORY,
+                ),
             )
-        )
 
-        hideUIKey = registerKeyMapping(
-            KeyMapping(
-                "key.dankhelper.hideUI",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_H,
-                CATEGORY
+        hideUIKey =
+            registerKeyMapping(
+                KeyMapping(
+                    "key.dankhelper.hideUI",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_H,
+                    CATEGORY,
+                ),
             )
-        )
-
-        moveUIKey = registerKeyMapping(
-            KeyMapping(
-                "key.dankhelper.move_ui",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_COMMA,
-                CATEGORY
+        moveUIKey =
+            registerKeyMapping(
+                KeyMapping(
+                    "key.dankhelper.moveUi",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_COMMA,
+                    CATEGORY,
+                ),
             )
-        )
 
-        clothConfigKey = registerKeyMapping(
-            KeyMapping(
-                "key.dankhelper.open_config",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_BACKSLASH,
-                CATEGORY
+        clothConfigKey =
+            registerKeyMapping(
+                KeyMapping(
+                    "key.dankhelper.openConfig",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_BACKSLASH,
+                    CATEGORY,
+                ),
             )
-        )
 
-        ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client ->
-            while (hideUIKey.consumeClick()) {
-                showUI = !showUI
-            }
-            while (resetSessionKey.consumeClick()) {
-                UtilFunctions.resetAll()
-            }
-            while (moveUIKey.consumeClick()) {
-                if (client.screen == null) {
-                    client.setScreen(EditHud())
+        ClientTickEvents.END_CLIENT_TICK.register(
+            ClientTickEvents.EndTick { client ->
+                while (hideUIKey.consumeClick()) {
+                    showUI = !showUI
                 }
-            }
-            while (clothConfigKey.consumeClick()) {
-                client.setScreen(buildConfigScreen(client.screen))
-           }
-        })
+                while (resetSessionKey.consumeClick()) {
+                    UtilFunctions.resetAll()
+                }
+                while (moveUIKey.consumeClick()) {
+                    if (client.screen == null) {
+                        client.setScreen(EditHud())
+                    }
+                }
+                while (clothConfigKey.consumeClick()) {
+                    client.setScreen(buildConfigScreen(client.screen))
+                }
+            },
+        )
     }
 }
