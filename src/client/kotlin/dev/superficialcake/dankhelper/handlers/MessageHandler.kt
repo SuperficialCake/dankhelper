ef10f0e9f14493d025ededcf348404ba0cbb7d5c
SuperficialCake <contact@superficialcake.dev>
2026-05-05 01:28:27 -0500

    Fixed Artifact regex finally?

--- src/client/kotlin/dev/superficialcake/dankhelper/handlers/MessageHandler.kt
+++ src/client/kotlin/dev/superficialcake/dankhelper/handlers/MessageHandler.kt
@@ -20,7 +20,7 @@ object MessageHandler {
     private val FORTUNE_PATTERN = """^\((.*)\) Increased Fortune: \+(\d+)""".toRegex()
     private val MOMENTUM_PATTERN = """^\((Enchants)\) Increased Momentum: \+(\d+)""".toRegex()
     private val ARTIFACT_PATTERN =
-        """^(\(Mining\)|\(Fishing\)|\(AutoMiner\)|\(OverDrive\)).*? (\d+)x (?!Random)(.*?) (Artifact)"""
+        """(?:(?:\((?:Mining|Fishing|AutoMiner|OverDrive)\)\s*(?:[Ff]ound\s*)?)|(?:-\s*))(\d+)x (?!Random)(.*?)\sArtifact(?:\s\(.*?\))?$"""
             .toRegex(RegexOption.IGNORE_CASE)
     private val RANKUP_PATTERN = """\(Rankup\).*?Cost:\s*\$?([\d,]+)""".toRegex()
     private val REWARDS_PATTERN = """.* has (Mined|Fished) ([\d]+)x (.*)""".toRegex()
@@ -30,12 +30,7 @@ object MessageHandler {
 
     private val logger = LoggerFactory.getLogger("dankhelper-chat")
 
-    private val username =
-        Minecraft
-            .getInstance()
-            .player
-            ?.displayName
-            .toString()
+    private val username = Minecraft.getInstance().gameProfile.name
     private val uuid = Minecraft.getInstance().gameProfile.id
     private val strippedUUID = uuid.toString().replace("-", "")
 
@@ -85,7 +80,7 @@ object MessageHandler {
 
             text.contains("Artifact") -> {
                 val matchArtifact = ARTIFACT_PATTERN.find(text) ?: return
-                val (_, amount) = matchArtifact.destructured
+                val (amount) = matchArtifact.destructured
 
                 StatsManager.addArtifact(amount.toLong())
                 logger.info("Found ${StatsManager.sumArtifact} this session")
