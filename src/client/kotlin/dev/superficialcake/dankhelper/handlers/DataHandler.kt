8620ce89cfc92bd959e2d43a5484c9cfcda13585
SuperficialCake <contact@superficialcake.dev>
2026-05-10 19:08:22 -0500

    Fixes multiple rollovers happening at UTC-0 00:00

--- src/client/kotlin/dev/superficialcake/dankhelper/handlers/DataHandler.kt
+++ src/client/kotlin/dev/superficialcake/dankhelper/handlers/DataHandler.kt
@@ -23,6 +23,7 @@ object DataHandler {
 
     private val DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd")
     private val TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss")
+    private var rolloverRegistered = false
 
     private fun getUtcDateTime(): ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC)
 
@@ -34,12 +35,16 @@ object DataHandler {
 
         prepareSessionFile()
         prepareTrendFile()
-        registerMidnightRollover()
+
+        if (!rolloverRegistered) {
+            registerMidnightRollover()
+        }
     }
 
     private fun registerMidnightRollover() {
         var lastRecordedDate = getUtcDateString()
         var tickCounter = 0
+        rolloverRegistered = true
 
         ClientTickEvents.END_CLIENT_TICK.register { _ ->
             tickCounter++
