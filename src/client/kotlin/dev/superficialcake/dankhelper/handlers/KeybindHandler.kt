7713271c477564ae537378c46397597b8f9902f9
SuperficialCake <contact@superficialcake.dev>
2026-05-11 21:41:50 -0500

    Improve Config screen
    showHUD saves on game close

--- src/client/kotlin/dev/superficialcake/dankhelper/handlers/KeybindHandler.kt
+++ src/client/kotlin/dev/superficialcake/dankhelper/handlers/KeybindHandler.kt
@@ -19,7 +19,8 @@ object KeybindHandler {
     lateinit var trendsUIKey: KeyBinding
 
     fun init() {
-        val config = AutoConfig.getConfigHolder(DankConfig::class.java).config
+        val holder = AutoConfig.getConfigHolder(DankConfig::class.java)
+        val config = holder.config
 
         resetSessionKey =
             registerKeyBinding(
@@ -75,6 +76,7 @@ object KeybindHandler {
             ClientTickEvents.EndTick { client ->
                 while (hideUIKey.wasPressed()) {
                     config.showHUD = !config.showHUD
+                    holder.save()
                 }
                 while (resetSessionKey.wasPressed()) {
                     UtilFunctions.resetAll()
