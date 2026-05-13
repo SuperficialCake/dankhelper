9039202d56ff2951da0192193de0b466cc19dda1
SuperficialCake <contact@superficialcake.dev>
2026-05-13 12:25:33 -0500

    Fix config screen not setting properly
    Version bump

--- src/client/kotlin/dev/superficialcake/dankhelper/handlers/KeybindHandler.kt
+++ src/client/kotlin/dev/superficialcake/dankhelper/handlers/KeybindHandler.kt
@@ -1,5 +1,6 @@
 package dev.superficialcake.dankhelper.handlers
 
+import dev.superficialcake.dankhelper.compat.buildConfigScreen
 import dev.superficialcake.dankhelper.config.DankConfig
 import dev.superficialcake.dankhelper.ui.EditHud
 import dev.superficialcake.dankhelper.ui.TrendsScreen
@@ -87,9 +88,7 @@ object KeybindHandler {
                     }
                 }
                 while (clothConfigKey.wasPressed()) {
-                    client.setScreen(
-                        AutoConfig.getConfigScreen(DankConfig::class.java, client.currentScreen).get(),
-                    )
+                    client.setScreen(buildConfigScreen(client.currentScreen))
                 }
                 while (trendsUIKey.wasPressed()) {
                     if (client.currentScreen == null) {
